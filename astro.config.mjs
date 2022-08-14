30e89e8ae6d544f9f2d1e929eb752a6c2e8fc73a
kris null <nikki@doge-mail.net>
2022-08-13 19:33:08 -0500

    cummiting this before my laptop fucking dies lmfao

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,15 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+// epic "github charges money to deploy shit on their piss-smelling servers"
+import { defineConfig } from 'astro/config';
+
+ import vercel from '@astrojs/vercel/serverless';
+
+export default defineConfig({
+
+   output: 'server',
+
+   adapter: vercel(),
+});
